c072c38bcfe7d1ac0f42eb526fab696a786a9b4a
realidademax <84300151+realidademax@users.noreply.github.com>
2021-06-09 22:06:33 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,491 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hmogiano-corredorexterno",
+      "name": "HMogiano CorredorExterno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.5051837122814717,
+        "pitch": -0.261241636509574,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8198209339976383,
+          "pitch": -0.18418223133852152,
+          "rotation": 5.497787143782138,
+          "target": "4-hmogiano-salaexamesi"
+        },
+        {
+          "yaw": -1.1804946982414481,
+          "pitch": 0.07478538524775402,
+          "rotation": 5.497787143782138,
+          "target": "1-hmogiano-salacomando-i"
+        },
+        {
+          "yaw": 2.450622538950187,
+          "pitch": -0.05689422065886873,
+          "rotation": 7.0685834705770345,
+          "target": "7-hmogiano-salatecnica-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hmogiano-salacomando-i",
+      "name": "HMogiano SalaComando I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.7106489567101626,
+        "pitch": -0.05375093083162419,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.885236551030089,
+          "pitch": -0.012045931140011845,
+          "rotation": 5.497787143782138,
+          "target": "4-hmogiano-salaexamesi"
+        },
+        {
+          "yaw": -1.962723280797583,
+          "pitch": -0.41558096883058937,
+          "rotation": 3.141592653589793,
+          "target": "2-hmogiano-salacomando-ii"
+        },
+        {
+          "yaw": 3.1259422182919003,
+          "pitch": -0.22592883852284906,
+          "rotation": 8.63937979737193,
+          "target": "3-hmogiano-salacomando-iii"
+        },
+        {
+          "yaw": 0.9899688245404334,
+          "pitch": 0.15987782834726616,
+          "rotation": 7.0685834705770345,
+          "target": "0-hmogiano-corredorexterno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hmogiano-salacomando-ii",
+      "name": "HMogiano SalaComando II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.0170448971833217,
+        "pitch": 0.13734821315764023,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7994636816946272,
+          "pitch": 0.0743348645815125,
+          "rotation": 3.141592653589793,
+          "target": "3-hmogiano-salacomando-iii"
+        },
+        {
+          "yaw": 2.0683796013781803,
+          "pitch": 0.47302165161322307,
+          "rotation": 8.63937979737193,
+          "target": "1-hmogiano-salacomando-i"
+        },
+        {
+          "yaw": -3.09044444819812,
+          "pitch": 0.10937501219613743,
+          "rotation": 0.7853981633974483,
+          "target": "4-hmogiano-salaexamesi"
+        },
+        {
+          "yaw": 2.001042278071255,
+          "pitch": 0.20540072135998244,
+          "rotation": 7.0685834705770345,
+          "target": "0-hmogiano-corredorexterno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hmogiano-salacomando-iii",
+      "name": "HMogiano SalaComando III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.749704957437089,
+        "pitch": 0.05938703861763983,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6507702287395976,
+          "pitch": 0.156769325317061,
+          "rotation": 3.141592653589793,
+          "target": "1-hmogiano-salacomando-i"
+        },
+        {
+          "yaw": 1.967321256075948,
+          "pitch": -0.05432340982861561,
+          "rotation": 3.141592653589793,
+          "target": "2-hmogiano-salacomando-ii"
+        },
+        {
+          "yaw": -2.4846870699843606,
+          "pitch": 0.22135780442371988,
+          "rotation": 7.853981633974483,
+          "target": "4-hmogiano-salaexamesi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hmogiano-salaexamesi",
+      "name": "HMogiano SalaExamesI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.4716691315313639,
+        "pitch": 0.02565738592856448,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.44545951600369804,
+          "pitch": -0.0819859183414664,
+          "rotation": 3.141592653589793,
+          "target": "5-hmogiano-salaexamesii"
+        },
+        {
+          "yaw": 1.5703728765991087,
+          "pitch": 0.20780491539646917,
+          "rotation": 0,
+          "target": "3-hmogiano-salacomando-iii"
+        },
+        {
+          "yaw": 2.6512604177401533,
+          "pitch": 0.062081276912856254,
+          "rotation": 0,
+          "target": "0-hmogiano-corredorexterno"
+        },
+        {
+          "yaw": -0.715046218185126,
+          "pitch": 0.008169915869192579,
+          "rotation": 0,
+          "target": "8-hmogiano-salatecnica-iii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hmogiano-salaexamesii",
+      "name": "HMogiano SalaExamesII",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.8942249302173053,
+        "pitch": 0.08643519610459194,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7733907633661614,
+          "pitch": 0.015022871708978869,
+          "rotation": 3.141592653589793,
+          "target": "4-hmogiano-salaexamesi"
+        },
+        {
+          "yaw": -2.203622514111565,
+          "pitch": 0.16616009442556745,
+          "rotation": 0,
+          "target": "8-hmogiano-salatecnica-iii"
+        },
+        {
+          "yaw": 2.2174931670990556,
+          "pitch": 0.04505258916488586,
+          "rotation": 5.497787143782138,
+          "target": "3-hmogiano-salacomando-iii"
+        },
+        {
+          "yaw": 2.5042226744839784,
+          "pitch": 0.00959304529704319,
+          "rotation": 0,
+          "target": "0-hmogiano-corredorexterno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-hmogiano-salatecnica-i",
+      "name": "HMogiano SalaTecnica I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.3267256359733466,
+        "pitch": 0.029861628239805427,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5730454967642924,
+          "pitch": 0.2695766660690957,
+          "rotation": 0,
+          "target": "7-hmogiano-salatecnica-ii"
+        },
+        {
+          "yaw": 2.7735864157342878,
+          "pitch": 0.2668190575585303,
+          "rotation": 3.141592653589793,
+          "target": "8-hmogiano-salatecnica-iii"
+        },
+        {
+          "yaw": -3.127821333435035,
+          "pitch": 0.02585465008113985,
+          "rotation": 0.7853981633974483,
+          "target": "5-hmogiano-salaexamesii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-hmogiano-salatecnica-ii",
+      "name": "HMogiano SalaTecnica II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.1174334485195025,
+        "pitch": -0.029861628239785887,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4135543829432606,
+          "pitch": 0.30928944744218434,
+          "rotation": 5.497787143782138,
+          "target": "6-hmogiano-salatecnica-i"
+        },
+        {
+          "yaw": 2.4828273235668465,
+          "pitch": 0.012749709208293325,
+          "rotation": 15.707963267948973,
+          "target": "8-hmogiano-salatecnica-iii"
+        },
+        {
+          "yaw": -0.8944353210006746,
+          "pitch": 0.1717002208023608,
+          "rotation": 0,
+          "target": "0-hmogiano-corredorexterno"
+        },
+        {
+          "yaw": 2.631676300987597,
+          "pitch": -0.2053295662708834,
+          "rotation": 7.853981633974483,
+          "target": "5-hmogiano-salaexamesii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hmogiano-salatecnica-iii",
+      "name": "HMogiano SalaTecnica III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.19608294980412921,
+        "pitch": -0.05375093083162419,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5625269770838415,
+          "pitch": 0.38922012124362126,
+          "rotation": 0.7853981633974483,
+          "target": "6-hmogiano-salatecnica-i"
+        },
+        {
+          "yaw": -0.8591403362531942,
+          "pitch": 0.153664229626159,
+          "rotation": 3.141592653589793,
+          "target": "7-hmogiano-salatecnica-ii"
+        },
+        {
+          "yaw": -3.102087796060218,
+          "pitch": -0.2512790085801555,
+          "rotation": 0.7853981633974483,
+          "target": "5-hmogiano-salaexamesii"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
